5f5e4f3ba472f23eaf2588c49146642baff6245e
TwinkelToe <63342939+TwinkelToe@users.noreply.github.com>
2020-04-17 13:13:38 +0200

    Create / Delete A Record

---
+++ Create-ARecord.ps1
@@ -0,0 +1,88 @@
+﻿# Infoblox Create A-record and corresponding PTR-record
+
+#VAR
+$a_record_to_create = "FQDN"
+$ip_addr = "127.0.0.1"
+$credential = Get-Credential
+
+#$password = ConvertTo-SecureString “PlainTextPassword” -AsPlainText -Force
+#$credential = New-Object System.Management.Automation.PSCredential (“username”, $password)
+
+#CONST
+$INFOBLOX_WAPI = "https://x.x.x.x/wapi/v2.9.5/"
+
+## Invoke-Rest kent geen -IgnoreInvalidSSL flag, disable SSL-check voor dit script.
+function DisableSSLCheck {
+Add-Type @"
+    using System;
+    using System.Net;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
+    public class ServerCertificateValidationCallback
+    {
+        public static void Ignore()
+        {
+            ServicePointManager.ServerCertificateValidationCallback += 
+                delegate
+                (
+                    Object obj, 
+                    X509Certificate certificate, 
+                    X509Chain chain, 
+                    SslPolicyErrors errors
+                )
+                {
+                    return true;
+                };
+        }
+    }
+"@
+ 
+    [ServerCertificateValidationCallback]::Ignore();
+    
+}
+DisableSSLCheck;
+
+$ReqeustURI = $INFOBLOX_WAPI + "record:a?name~=" + $a_record_to_create
+"Get URL: $ReqeustURI"
+$Result_a_record = Invoke-RestMethod -Method Get -Uri $ReqeustURI -Credential $Credential
+
+if ($Result_a_record){
+    Write-Host "A-record already exists:" -BackgroundColor Red
+    $Result_a_record
+} else {
+    # Create A-record
+    Write-Host "No A-record found, creating.." -BackgroundColor Green
+    $ReqeustURI = $INFOBLOX_WAPI + "record:a"
+
+    $JsonBody = '
+    {
+    	"name": "' + $a_record_to_create + '",
+    	"ipv4addr": "' + $ip_addr + '"
+    }
+    '
+
+    $Result_a_post = Invoke-RestMethod -Method Post -Uri $ReqeustURI -Body $JsonBody -Credential $Credential -ContentType "application/json"
+    "Result of post: $Result_a_post"
+    if ($Result_a_post){
+        # Create PTR-Record
+        $ReqeustURI = $INFOBLOX_WAPI + "record:ptr?ptrdname~=" + $a_record_to_remove
+        "Get URL: $ReqeustURI"
+        $Result_ptr_record = Invoke-RestMethod -Method Get -Uri $ReqeustURI -Credential $Credential
+        if ($Result_ptr_record){
+            write-host "PTR-record already exist:" -BackgroundColor Red
+            $Result_ptr_record
+        } else {
+                Write-Host "No PTR-record found, creating.." -BackgroundColor Green
+            $ReqeustURI = $INFOBLOX_WAPI + "record:ptr"
+
+            $JsonBody = '
+            {
+    	        "ptrdname": "' + $a_record_to_create + '",
+    	        "ipv4addr": "' + $ip_addr + '"
+            }
+            '
+            $Result_ptr_post = Invoke-RestMethod -Method Post -Uri $ReqeustURI -Body $JsonBody -Credential $Credential -ContentType "application/json"
+            "Result of post: $Result_ptr_post"
+        }
+    }
+}
---
+++ Delete-ARecord.ps1
@@ -0,0 +1,73 @@
+﻿# Infoblox delete a-record and corresponding PTR-record
+
+#VAR
+$a_record_to_remove = "FQDN"
+$credential = Get-Credential
+$commit_changes = $false #Commit changes? Functions as otherwise functions as -whatif
+
+#$password = ConvertTo-SecureString “PlainTextPassword” -AsPlainText -Force
+#$credential = New-Object System.Management.Automation.PSCredential (“username”, $password)
+
+#CONST
+$INFOBLOX_WAPI = "https://x.x.x.x/wapi/v2.9.5/"
+
+## Invoke-Rest kent geen -IgnoreInvalidSSL flag, disable SSL-check voor dit script.
+function DisableSSLCheck {
+Add-Type @"
+    using System;
+    using System.Net;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
+    public class ServerCertificateValidationCallback
+    {
+        public static void Ignore()
+        {
+            ServicePointManager.ServerCertificateValidationCallback += 
+                delegate
+                (
+                    Object obj, 
+                    X509Certificate certificate, 
+                    X509Chain chain, 
+                    SslPolicyErrors errors
+                )
+                {
+                    return true;
+                };
+        }
+    }
+"@
+ 
+    [ServerCertificateValidationCallback]::Ignore();
+    
+}
+DisableSSLCheck;
+
+# Delete A record
+$ReqeustURI = $INFOBLOX_WAPI + "record:a?name~=" + $a_record_to_remove
+"Get URL: $ReqeustURI"
+$Result_a_record = Invoke-RestMethod -Method Get -Uri $ReqeustURI -Credential $Credential
+$Result_a_record
+
+if (-not $Result_a_record){
+    Write-Host "No A record found." -BackgroundColor Red
+} elseif ($commit_changes -and ($Result_a_record.Length -eq 1)) {
+    Write-Host "Deleting A-record..." -BackgroundColor Green
+    $ReqeustURI = $INFOBLOX_WAPI + $Result_a_record._ref
+    $Result_of_delete = Invoke-RestMethod -Method Delete -Uri $ReqeustURI -Credential $Credential
+    "Result of delete: $Result_of_delete"
+}
+
+# Delete PTR Record
+$ReqeustURI = $INFOBLOX_WAPI + "record:ptr?ptrdname~=" + $a_record_to_remove
+"Get URL: $ReqeustURI"
+$Result_ptr_record = Invoke-RestMethod -Method Get -Uri $ReqeustURI -Credential $Credential
+$Result_ptr_record
+
+if (-not $Result_ptr_record){
+    Write-Host "No PTR record found." -BackgroundColor Red
+} elseif ($commit_changes -and ($Result_ptr_record.Length -eq 1)) {
+    Write-Host "Deleting PTR-record..." -BackgroundColor Green
+    $ReqeustURI = $INFOBLOX_WAPI + $Result_ptr_record._ref
+    $Result_ptr_delete = Invoke-RestMethod -Method Delete -Uri $ReqeustURI -Credential $Credential
+    "Result of PTR delete: $Result_ptr_delete"
+}
